ba6139f190cea0768a051b9cf9478bed04185b15
Sarah Cadet <SCadet1@ucvts.tec.nj.us>
2019-10-22 13:16:43 -0400

    perimeter

---
+++ src/perimeter.js
@@ -0,0 +1,12 @@
+const readlineSync = require("readline-sync");
+
+const a = readlineSync.question("Width: ");
+const b = readlineSync.question("Length: ");
+const c = a * 2.54
+const d = b * 2.54
+const e = c * 2
+const f = d * 2
+const g = e + f
+const h = g.toFixed(2);
+
+console.log("A(n) " + a + "-by-" + b + "-inch sheet of paper has a perimeter of " + g + " centimeter(s).")
