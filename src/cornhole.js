54e2ded829c1e342ef8cfba625d0ed4cdf056fd9
Sarah Cadet <SCadet1@ucvts.tec.nj.us>
2019-10-28 13:28:34 -0400

    cornhole

---
+++ src/cornhole.js
@@ -0,0 +1,8 @@
+const readlineSync = require("readline-sync");
+
+const area = 48 * 24
+const circle = 3.14 * 3
+const circle2 = circle**2
+const final = area - circle2
+
+console.log("\nThe surface area of a standard Cornhole board is 1,123.73 square inch(es).")
